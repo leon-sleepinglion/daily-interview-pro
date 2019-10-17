cf3995e00f668ec5f7411eaa4d47c6562a1fb201
Leon Wee <leonweecs@gmail.com>
2019-10-17 21:22:49 +1300

    add solution for 012

---
+++ 012 Invert a Binary Tree/main.py
@@ -0,0 +1,30 @@
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+  def preorder(self):
+    print(self.value)
+    if self.left: self.left.preorder()
+    if self.right: self.right.preorder()
+
+def invert(node):
+  if not node:
+    return
+  node.left, node.right = node.right, node.left
+  invert(node.left)
+  invert(node.right)
+
+root = Node('a') 
+root.left = Node('b') 
+root.right = Node('c') 
+root.left.left = Node('d') 
+root.left.right = Node('e') 
+root.right.left = Node('f') 
+
+root.preorder()
+# a b d e c f 
+print("\n")
+invert(root)
+root.preorder()
+# a c f b e d
